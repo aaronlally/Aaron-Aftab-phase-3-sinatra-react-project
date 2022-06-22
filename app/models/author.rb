b93f88342b2ec7486c575147f9edd720c048fb85
Aaron Lally <aaronblally@gmail.com>
2022-06-22 15:00:17 -0400

    query changes

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,17 +2,17 @@ class Author < ActiveRecord::Base
     has_many :books
 
     def self.alphabetical_order
-        all.order(:name)
+        self.order(:name)
     end
 
     def self.order_by_birth_year
-        all.order(:birth_year)
+        self.order(:birth_year)
     end
 
     def self.oldest_author
-        all.order(:birth_year).last
+        self.order(:birth_year).last
     end
     def self.youngest_author
-        all.order(:birth_year).first
+        self.order(:birth_year).first
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,24 +2,24 @@ class Book < ActiveRecord::Base
     belongs_to :author
 
     def self.alphabetical_order
-        all.order(:title)
+        self.order(:title)
     end
     def self.order_by_rating
-        all.order(:rating)
+        self.order(:rating)
     end
     def self.order_by_release_date
-        all.order(:release_date)
+        self.order(:release_date)
     end
     def self.highest_rating
-        all.order(:rating).last
+        self.order(:rating).last
     end
     def self.lowest_rating
-        all.order(:rating).first
+        self.order(:rating).first
     end
     def self.popular_books
-        all.where("rating > ?", 5)
+        self.where("rating > ?", 5)
     end
     def self.unpopular_books
-        all.where("rating < ?", 5)
+        self.where("rating < ?", 5)
     end
 end
