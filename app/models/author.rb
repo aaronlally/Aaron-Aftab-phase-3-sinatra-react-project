c387f59e82a5c688b3494ad104c02e88399dbb0b
Aaron Lally <aaronblally@gmail.com>
2022-06-22 13:20:36 -0400

    added query methods for author and book

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,4 +5,14 @@ class Author < ActiveRecord::Base
         all.order(:name)
     end
 
+    def self.order_by_birth_year
+        all.order(:birth_year)
+    end
+
+    def self.oldest_author
+        all.order(:birth_year).last
+    end
+    def self.youngest_author
+        all.order(:birth_year).first
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,25 @@
 class Book < ActiveRecord::Base
     belongs_to :author
 
-
+    def self.alphabetical_order
+        all.order(:title)
+    end
+    def self.order_by_rating
+        all.order(:rating)
+    end
+    def self.order_by_release_date
+        all.order(:release_date)
+    end
+    def self.highest_rating
+        all.order(:rating).last
+    end
+    def self.lowest_rating
+        all.order(:rating).first
+    end
+    def self.popular_books
+        all.where("rating > ?", 5)
+    end
+    def self.unpopular_books
+        all.where("rating < ?", 5)
+    end
 end
