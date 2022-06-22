bcb9bf59a925dbc0b23000fae1e2629540d51ef6
Aaron Lally <aaronblally@gmail.com>
2022-06-22 13:05:04 -0400

    added get post patch delete for book and added get and post for author in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,49 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/books" do
+    books = Book.all
+    books.to_json
+  end
+
+  post "/add_book" do
+    book = Book.create(
+      title: params[:title],
+      rating: params[:rating],
+      release_date: params[:release_date],
+      author_id: params[:author_id]
+    )
+    book.to_json
+  end
+
+  patch "/book/:id" do
+    book = Book.find(params[:id])
+    book.update(
+      title: params[:title],
+      rating: params[:rating],
+      release_date: params[:release_date],
+      author_id: params[:author_id]
+    )
+    book.to_json
+  end
+
+  delete "/book/:id" do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end
+
+  get "/authors" do
+    authors = Author.all
+    authors.to_json
+  end
+
+  post "/add_author" do
+    author = Author.create(
+      name: params[:name],
+      birth_year: params[:birth_year]
+    )
+    author.to_json
   end
 
 end
